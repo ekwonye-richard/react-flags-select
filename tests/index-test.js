b29ef91a23c45d73cd52362d66ae21dea10c2fb1
ekwonye <ekwonye>
2017-05-07 20:02:15 +0100

    modified test

--- tests/index-test.js
+++ tests/index-test.js
@@ -15,9 +15,9 @@ describe('Component', () => {
     unmountComponentAtNode(node)
   })
 
-  it('displays a welcome message', () => {
+  it('renders ', () => {
     render(<Component/>, node, () => {
-      expect(node.innerHTML).toContain('Welcome to React components')
+      expect(node).toExist()
     })
   })
 })
