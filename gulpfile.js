834dd797dceaf423d8574a1260aca8656605f75d
chinedum.ekwonye <chinedum.ekwonye>
2017-05-09 15:09:37 +0100

    compiling and importing css file

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
+  return gulp.src('./scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./scss/**/*.scss', ['sass']);
 });
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 import countries from './countries';
-import '../scss/style.scss';
+import '../css/style.css';
 
 class ReactFlagsSelect extends Component {
 	constructor(props){
