358bce0d4fc7a7f0bbbaff30a131497695d94aa8
ekwonye <ekwonye>
2017-05-15 22:57:19 +0100

    component update

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,22 +1,50 @@
 import React from 'react'
 import {render} from 'react-dom'
+import '../../css/demo.css';
 import '../../css/react-flags-select.css';
 
 import ReactFlagsSelect from '../../src'
 
-let Demo = React.createClass({
-  render() {
-    return <div>
-      <h1>react-flags-select Demo</h1>
-      <ReactFlagsSelect
-		countries={["US", "GB", "FR","DE","IT"]}
-		customLabels={{"US": "EN-US","GB": "EN-GB","FR": "FR","DE": "DE","IT": "IT"}}
-		placeholder="Select Language"
-		selectedSize={14}
-		optionsSize={14}
-		showSelectedLabel={false} />
-    </div>
-  }
-})
+let Remarkable = require('remarkable');
+
+class Markdown extends React.Component {
+	render() {
+		var md = new Remarkable();
+
+		return (
+			<div
+	          className="content"
+	          dangerouslySetInnerHTML={{ __html: md.render(this.props.children) }}
+	        />
+		)
+	}
+}
+
+class Demo extends React.Component {
+	render() {
+		return (
+			<div>
+				<div className="header">
+					<span className="title">React Flags Select</span>
+				</div>
+				<div className="main">
+					<div className="section-header">
+						<span>Examples</span>
+					</div>
+					<hr />
+					<ReactFlagsSelect
+					countries={["US", "GB", "FR","DE","IT"]}
+					customLabels={{"US": "EN-US","GB": "EN-GB","FR": "FR","DE": "DE","IT": "IT"}}
+					placeholder="Select Language"
+					selectedSize={14}
+					optionsSize={14}
+					showSelectedLabel={false} />
+				</div>
+			</div>
+		)
+	}
+}
+
+
 
 render(<Demo/>, document.querySelector('#demo'))
