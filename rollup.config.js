81ac08186f179aa01275aa99087d21d66eb6850f
Richard Ekwonye <ekwonyenoob@gmail.com>
2021-01-03 22:31:01 +0000

    chore: add rollup to bundle library

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup//plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import postcss from "rollup-plugin-postcss";
+
+import pkg from "./package.json";
+
+const input = "src/index.ts";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: pkg.module,
+      format: "esm",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    babel({
+      exclude: "node_modules/**",
+      presets: ["@babel/env", "@babel/preset-react"],
+    }),
+    commonjs({ exclude: "/src/components/Flags/Af.js" }),
+    typescript({ useTsconfigDeclarationDir: true }),
+    postcss(),
+  ],
+};
