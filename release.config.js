ae37c53009c9e105a4e03fcfcbea8ed6a8086a18
Eyo Okon Eyo <maziey93@gmail.com>
2021-01-17 19:41:58 +0100

    chore: automate package release automation

---
+++ release.config.js
@@ -0,0 +1,29 @@
+import pkg from "./package.json";
+
+module.exports = {
+  ci: false,
+  debug: true,
+  dryRun: true,
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/npm",
+      {
+        assets: [...pkg.files, "package.json", "yarn.lock", "CHANGELOG.md"],
+      },
+    ],
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json", "yarn.lock", "CHANGELOG.md"],
+      },
+    ],
+  ],
+};
